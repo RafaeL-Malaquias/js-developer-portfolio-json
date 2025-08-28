d19d2cb813e53b8ee4b5a9dd4e0dff6fadf6551b
rafael <fael.oliveira.3@hotmail.com>
2025-08-28 20:41:24 +0100

    .

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,7 +2,7 @@
 
 
 async function fetchProfileData() {
- const url = 'https://raw.githubusercontent.com/RafaeL-Malaquias/js-developer-portfolio/refs/heads/main/data/profile.json?token=GHSAT0AAAAAAC3SS5B4KXM2OETJMJPSCUJI2FQWTOA';
+ const url = 'https://raw.githubusercontent.com/RafaeL-Malaquias/js-developer-portfolio/refs/heads/main/data/profile.json?token=GHSAT0AAAAAAC3SS5B4PQH5QVUKQ7GI2HOG2FQXCUQ';
     const fetching = await fetch(url)
     return await fetching.json()
 }
